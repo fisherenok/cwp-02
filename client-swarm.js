70c95971fb83ab01b5a2568945fe37f139c71092
Fisher <domenikfail@gmail.com>
2020-01-30 12:22:55 +0300

    add clSwarm

---
+++ client-swarm.js
@@ -0,0 +1,13 @@
+const childProcess = require('child_process');
+let n = process.argv[2];
+
+for (;n > 0; n--) {
+    console.log(`number client -> ${n}`);
+    childProcess.exec('node client.js', (err, stdout) => {
+        if (err) {
+            throw err;
+        } else {
+            console.log(`<------------------>\n${stdout}`);
+        }
+    })
+}
